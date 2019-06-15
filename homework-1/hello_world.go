1b0a5aae1dfcb54287269ad281e9640db4c96efe
u2 <u2@lenta.ru>
2019-06-15 23:02:35 +0500

    added conv

--- homework-1/hello_world.go
+++ homework-1/hello_world.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Привет, мир!")
+	//    fmt.Println("Привет, мир!")
+	Conv()
+}
+
+func Conv() {
+	var rsum int8
+	const scrs int8 = 64
+	fmt.Println("Сумма в рублях?")
+	fmt.Scanln(&rsum)
+	fmt.Println("Сумма $", rsum/scrs)
 }
