73316273d3fbc0d143847947982489fecace386f
u2 <u2@lenta.ru>
2019-06-16 16:18:25 +0500

    Добавлены функции 	currConv() 	triangleSquare() 	depositSum()

--- homework-1/homework-1.go
+++ homework-1/homework-1.go
@@ -4,13 +4,13 @@ import "fmt"
 import "math"
 
 func main() {
-	//    fmt.Println("Привет, мир!")
-	Conv()
+	currConv()
 	triangleSquare()
+	depositSum()
 }
 
-// Выводит сумму в $
-func Conv() {
+//Выводит сумму в $
+func currConv() {
 	var rsum int
 	const scrs int = 64
 	fmt.Println("Сумма в рублях?")
@@ -19,10 +19,24 @@ func Conv() {
 }
 
 //Вычисляет площадь, периметр и гипотенузу прямоугольного треугольника.
-func triangleSquare()
-{
-	var	leg1, leg2 float64 = 2,3
+func triangleSquare() {
+	var leg1, leg2 float64 = 3, 4
 	var hyp, sq, perimeter float64
-	hyp = math.Sqrt(math.Pow(leg1,2)+math.Pow(leg2,2))
-	fmt.Println("Гипотенуза: ",hyp)
+	hyp = math.Sqrt(math.Pow(leg1, 2) + math.Pow(leg2, 2))
+	fmt.Println("Гипотенуза: ", hyp)
+	sq = leg1 * leg2 / 2
+	fmt.Println("Площадь: ", sq)
+	perimeter = leg1 + leg2 + hyp
+	fmt.Println("Периметр: ", perimeter)
+}
+
+//Вычисляет сумму вклада
+func depositSum() {
+	var startDep, endDep, percent float64
+	fmt.Println("Начальная сумма вклада?")
+	fmt.Scanln(&startDep)
+	fmt.Println("Годовой процент?")
+	fmt.Scanln(&percent)
+	endDep = startDep * math.Pow(1+percent/100, 5)
+	fmt.Println("Сумма вклада через 5 лет: ", endDep)
 }
