a0604d30c476e1b325e9c65aa8cfa14f0e0abc18
u2 <u2@lenta.ru>
2019-06-19 17:17:43 +0500

    Добавлены parityChecking и divByTreeChicking

--- homework-2/homework-2.go
+++ homework-2/homework-2.go
@@ -1,5 +1,38 @@
 package main
 
-func main(){
-	
+import "fmt"
+
+func main() {
+	parityChecking()
+	divByTreeChicking()
+	seqFibonacci100()
+}
+
+//Проверка чётности
+func parityChecking() {
+	var i int
+	fmt.Println("Введите число для проверки")
+	fmt.Scanln(&i)
+	if i%2 == 0 {
+		fmt.Println(i, "чётное")
+	} else {
+		fmt.Println(i, "нечётное")
+	}
+}
+
+//Проверка деления на 3
+func divByTreeChicking() {
+	var i int
+	fmt.Println("Введите число для проверки")
+	fmt.Scanln(&i)
+	if i%3 == 0 {
+		fmt.Println("делится на 3 без остатка")
+	} else {
+		fmt.Println("не делится на 3 без остатка")
+	}
+}
+
+//Вывод первых 100 чисел последовательности Фибоначчи
+func seqFibonacci100() {
+	//defer, panic, recover
 }
