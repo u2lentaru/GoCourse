dce17aa2e9a25839bf9df1dd706e488bfb941d77
u2 <u2@lenta.ru>
2019-06-20 17:31:09 +0500

    Добавлена функция seqFibonacci100

--- homework-2/homework-2.go
+++ homework-2/homework-2.go
@@ -26,13 +26,34 @@ func divByTreeChecking() {
 	fmt.Println("Введите число для проверки")
 	fmt.Scanln(&i)
 	if i%3 == 0 {
-		fmt.Println("делится на 3 без остатка")
+		fmt.Println(i, "делится на 3 без остатка")
 	} else {
-		fmt.Println("не делится на 3 без остатка")
+		fmt.Println(i, "не делится на 3 без остатка")
 	}
 }
 
 //Вывод первых 100 чисел последовательности Фибоначчи
 func seqFibonacci100() {
-	//defer, panic, recover
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+		}
+	}()
+	fmt.Println("1 число Фибоначчи 1")
+	fmt.Println("2 число Фибоначчи 1")
+	var fnumb uint64 = 2
+	fib(1, 1, &fnumb)
+}
+
+//рекурсия
+func fib(f1, f2 uint64, pfnumb *uint64) {
+	f := f1
+	f1 = f2
+	f2 = f + f1
+	fmt.Println(*pfnumb, "число Фибоначчи", f2)
+	*pfnumb++
+	if *pfnumb >= 100 {
+		panic(*pfnumb)
+	}
+	fib(f1, f2, pfnumb)
 }
