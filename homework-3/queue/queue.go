5dfb44057e901541b3f2aa3103556f04b6220692
u2 <u2@lenta.ru>
2019-06-23 15:17:45 +0500

    Added functions Push(str string), Pop()

--- homework-3/queue/queue.go
+++ homework-3/queue/queue.go
@@ -1,3 +1,20 @@
 package queue
 
 var q []string
+
+//Push add new element to the queue.
+func Push(str string) {
+	q = append(q, str)
+}
+
+//Pop returns first element added to the queue.
+func Pop() string {
+	lq := len(q)
+	if lq == 0 {
+		return ""
+	}
+
+	popElem := q[0]
+	q = q[1:lq]
+	return popElem
+}
