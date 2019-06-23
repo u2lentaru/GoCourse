7bdaaab4b26354b68ca14664fdcff318bbcfcaa0
u2 <u2@lenta.ru>
2019-06-23 14:42:59 +0500

    Add function carsAndTrucksStracts()

--- homework-3/homework-3.go
+++ homework-3/homework-3.go
@@ -1,5 +1,33 @@
 package main
 
-func main {
-	
+import "fmt"
+
+func main() {
+	carsAndTrucksStracts()
+}
+
+//Creating and comparing cars and trucks data structures
+func carsAndTrucksStracts() {
+
+	type car struct {
+		model string
+		year  int
+		color string
+	}
+
+	type truck struct {
+		model        string
+		year         int
+		loadCapacity float32
+	}
+
+	a := car{"Ford Focus", 2014, "white"}
+	b := car{year: 2014, model: "Ford Focus", color: "white"}
+	c := car{"Toyota Camry", 2012, "black"}
+	t := truck{"Volvo", 2014, 12000}
+
+	t.loadCapacity = 16000
+	fmt.Println(t, a.color)
+	fmt.Println("a==b", a == b)
+	fmt.Println("a==c", a == c)
 }
