6a115854247e17fa60adebaa30088dbac495bad1
u2 <u2@lenta.ru>
2019-06-27 17:29:42 +0500

    Added sortAddressBook()

--- homework-4/homework-4.go
+++ homework-4/homework-4.go
@@ -4,10 +4,12 @@ import (
 	"calculator"
 	"fmt"
 	"math"
+	"sort"
 )
 
 func main() {
 	calcCall()
+	sortAddressBook()
 }
 
 type perimeter interface {
@@ -28,6 +30,50 @@ type circle struct {
 
 func (c circle) perimeter() float64 {
 	return 2 * math.Pi * c.radius
+}
+
+type addressBook struct {
+	abName string
+	//	abPhones []int
+	abPhones int
+}
+
+type utAddressBook []addressBook
+
+func (u utAddressBook) Len() int {
+	return len(u)
+}
+
+func (u utAddressBook) Swap(i, j int) {
+	u[i], u[j] = u[j], u[i]
+}
+
+func (u utAddressBook) Less(i, j int) bool {
+	return u[i].abName < u[j].abName
+}
+
+//addressBook := make(map[string][]int)
+//addressBook["Bob2"] = []int{89167243813}
+//addressBook["Bob2"] = append(addressBook["Bob2"], 89155243628)
+
+//func (a ByAge) Len() int           { return len(a) }
+//func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+//func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
+
+func sortAddressBook() {
+	u := utAddressBook{
+		{"Bob", 89167243814},
+		{"Alex", 89155243629},
+	}
+
+	fmt.Println(u)
+	sort.Sort(utAddressBook(u))
+	fmt.Println(u)
+	//u[0].abName = "Alex"
+	//u[0].abPhones = [89167243813]
+	//u[1].abName = "Bob"
+	//u[1].abPhones = [89155243628]
+
 }
 
 func calcCall() {
