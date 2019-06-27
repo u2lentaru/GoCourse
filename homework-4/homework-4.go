2a36feba0b06d3c5df1256506b9652bc1d05e79f
u2 <u2@lenta.ru>
2019-06-27 14:47:45 +0500

    homework-4.go has been modified

--- homework-4/homework-4.go
+++ homework-4/homework-4.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"calculator"
+	"fmt"
+)
+
 func main() {
 	calcCall()
+}
+
+func calcCall() {
+	input := ""
+	for {
+		fmt.Print("> ")
+		if _, err := fmt.Scanln(&input); err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		if input == "exit" {
+			break
+		}
+
+		if res, err := calculator.Calculate(input); err == nil {
+			fmt.Printf("Результат: %v\n", res)
+		} else {
+			fmt.Println("Не удалось произвести вычисление")
+		}
+	}
 }
