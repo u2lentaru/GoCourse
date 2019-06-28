361961d2a0a6c81ec3d3b8fb1dd9a3d4ef13b8be
u2 <u2@lenta.ru>
2019-06-28 16:45:45 +0500

    Added chessKnightMoves

--- homework-4/homework-4.go
+++ homework-4/homework-4.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	calcCall()
 	sortAddressBook()
+	fmt.Println("Possible chess knight moves from {3,3}", chessKnightMoves(3, 3))
 }
 
 //New interface and several structures that satisfy it.
@@ -60,9 +61,10 @@ func sortAddressBook() {
 	u := utAddressBook{
 		{"Bob", []int{89167243814}},
 		{"Alex", []int{89155243629, 89155243630}},
+		{"Mike", []int{89167245643}},
 	}
 
-	fmt.Println("Befor sorting", u)
+	fmt.Println("Before sorting", u)
 	sort.Sort(u)
 	fmt.Println("After sorting", u)
 }
@@ -94,11 +96,54 @@ func calcCall() {
 	}
 }
 
+//structure for chessKnightMoves
+type cbPoint struct {
+	x int
+	y int
+}
+
+func (cbp *cbPoint) setPoint(spx, spy int) {
+	cbp.x = spx
+	cbp.y = spy
+}
+
+func (cbp cbPoint) chessKnightPoints() []cbPoint {
+	mycbp := make([]cbPoint, 0)
+	var a cbPoint
+
+	for i := -2; i <= 2; i++ {
+		for j := -2; j <= 2; j++ {
+			if i != j && i != -j && i != 0 && j != 0 {
+				a.x = cbp.x + i
+				a.y = cbp.y + j
+				if a.x >= 1 && a.x <= 8 && a.y >= 1 && a.y <= 8 {
+					mycbp = append(mycbp, a)
+				}
+
+			}
+		}
+
+	}
+
+	return mycbp
+}
+
+//chessKnightMoves returns possible chess knight moves.
+func chessKnightMoves(kx, ky int) []cbPoint {
+
+	var ckp cbPoint
+	var sckp []cbPoint
+	ckp.setPoint(kx, ky)
+	sckp = ckp.chessKnightPoints()
+	return sckp
+}
+
 //Hello from init
-//> 2+5
+//> 5+2
 //Result: 7
 //> help
 //Program calculates the data of the expression passed in the string. Type 'help' for help, 'exit' for exit.
 //> exit
-//Befor sorting [{Bob [89167243814]} {Alex [89155243629 89155243630]}]
-//After sorting [{Alex [89155243629 89155243630]} {Bob [89167243814]}]
+//Before sorting [{Bob [89167243814]} {Alex [89155243629 89155243630]} {Mike [89167245643]}]
+//After sorting [{Alex [89155243629 89155243630]} {Bob [89167243814]} {Mike [89167245643]}]
+//Possible chess knight moves from {3,3} [{1 2} {1 4} {2 1} {2 5} {4 1} {4 5} {5 2} {5 4}]
