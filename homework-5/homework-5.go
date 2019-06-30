ad359ef62ee601edcadbbf89095c26ab619f7476
u2 <u2@lenta.ru>
2019-06-30 16:27:58 +0500

    rwCSV() has been modified

--- homework-5/homework-5.go
+++ homework-5/homework-5.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"log"
 	"os"
 )
 
@@ -53,10 +52,10 @@ func fileRead() {
 
 func rwCSV() {
 	records := [][]string{
-		{"first_name", "last_name", "username"},
-		{"Rob", "Pike", "rob"},
-		{"Ken", "Thompson", "ken"},
-		{"Robert", "Griesemer", "gri"},
+		{"name", "phone"},
+		{"Bob", "89167243814"},
+		{"Alex", "89155243629"},
+		{"Mike", "89167245643"},
 	}
 
 	wfile, err := os.Create("somecsvfile.csv")
@@ -67,32 +66,32 @@ func rwCSV() {
 	defer wfile.Close()
 
 	w := csv.NewWriter(wfile)
-	w.WriteAll(records) // calls Flush internally
+	w.WriteAll(records)
 
 	if err := w.Error(); err != nil {
-		log.Fatalln("error writing csv:", err)
-
-		rfile, err := os.Open("somecsvfile.csv")
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		defer rfile.Close()
-
-		reader := csv.NewReader(rfile)
-		reader.FieldsPerRecord = -1 // see the Reader struct information below
-		rawCSVdata, err := reader.ReadAll()
-
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// sanity check, display to standard output
-		for _, each := range rawCSVdata {
-			fmt.Printf("email : %s and timestamp : %s\n", each[0], each[1])
-		}
+		fmt.Println("error writing csv:", err)
+		return
 	}
+	rfile, err := os.Open("somecsvfile.csv")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer rfile.Close()
+
+	reader := csv.NewReader(rfile)
+	rawCSVdata, err := reader.ReadAll()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// sanity check, display to standard output
+	for _, each := range rawCSVdata {
+		fmt.Printf("%s and %s\n", each[0], each[1])
+	}
+
 }
