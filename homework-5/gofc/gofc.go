af2bc9ec709e849c45177cdeedbfd5ca394c817b
u2 <u2@lenta.ru>
2019-06-30 22:34:43 +0500

    gofc.go has been modifie

--- homework-5/gofc/gofc.go
+++ homework-5/gofc/gofc.go
@@ -1,6 +1,27 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+)
+
 func main() {
 
-}
+	srcptr := flag.String("src", "", "source file")
+	dstptr := flag.String("dst", "", "destination file")
 
+	fmt.Println("flags", *srcptr, *dstptr)
+
+	content, err := ioutil.ReadFile(*srcptr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = ioutil.WriteFile(*dstptr, content, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
