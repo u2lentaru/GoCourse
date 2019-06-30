006828d033d78acb2313a3692684b7aabd87a308
u2 <u2@lenta.ru>
2019-06-30 22:59:49 +0500

    gogrep.go has been modified

--- homework-5/gogrep/gogrep.go
+++ homework-5/gogrep/gogrep.go
@@ -14,4 +14,14 @@ func main() {
 	fmt.Println(query)
 	fmt.Println(root)
 	fmt.Println(os.Args)
+
+	filepath.Walk(root, func(path string, file os.FileInfo, err error) error {
+		if !file.IsDir() {
+			for i := 1; scanner.Scan(); i++ {
+				if strings.Contains(scanner.Text(), query) {
+					found = 0
+					fmt.Printf("%s/%s:%d: %s\n", root, path, i, scanner.Text())
+				}
+		}
+		return nil
 }
