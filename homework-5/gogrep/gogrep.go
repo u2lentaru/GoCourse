577cde71c0678dfccdb4db252b6a91a793c4420e
u2 <u2@lenta.ru>
2019-06-30 22:42:14 +0500

    gogrep.go has been modifie

--- homework-5/gogrep/gogrep.go
+++ homework-5/gogrep/gogrep.go
@@ -1,6 +1,17 @@
 package main
 
-func main() {
-	
-}
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	query := os.Args[0]
+	root := os.Args[1]
+
+	//arg := os.Args[3]
+
+	fmt.Println(query)
+	fmt.Println(root)
+	fmt.Println(os.Args)
+}
