0cf9ccf497bdca2e42d9b29ff10651cc6fd655f6
u2 <u2@lenta.ru>
2019-07-01 16:18:21 +0500

    gogrep.go has been modified

--- homework-5/gogrep/gogrep.go
+++ homework-5/gogrep/gogrep.go
@@ -8,17 +8,12 @@ import (
 	"strings"
 )
 
+var found = 1
+
 func main() {
 	query := os.Args[2]
 	root := os.Args[1]
-	found := 1
-
-	fmt.Println("query", query)
-	fmt.Println("root", root)
-	//	fmt.Println("os.Args", os.Args)
-
 	filepath.Walk(root, func(path string, file os.FileInfo, err error) error {
-		//		fmt.Println(path)
 		if !file.IsDir() {
 			file, err := os.Open(path)
 			defer file.Close()
@@ -35,17 +30,9 @@ func main() {
 		}
 		return nil
 	})
-
-	/*	filepath.Walk(root, func(path string, file os.FileInfo, err error) error {
-			if !file.IsDir() {
-				for i := 1; scanner.Scan(); i++ {
-					if strings.Contains(scanner.Text(), query) {
-						found = 0
-						fmt.Printf("%s/%s:%d: %s\n", root, path, i, scanner.Text())
-					}
-			}
-			return nil
-		}
-		}
-	*/
 }
+
+//PS C:\Golang_work\src\GoCourse\homework-5\gogrep> go run .\gogrep.go C:\Golang_work\src\GoCourse\homework-5 тест
+//C:\Golang_work\src\GoCourse\homework-5/C:\Golang_work\src\GoCourse\homework-5\gofc\test.txt:2: тест
+//C:\Golang_work\src\GoCourse\homework-5/C:\Golang_work\src\GoCourse\homework-5\homework-5.go:104: //тест
+//C:\Golang_work\src\GoCourse\homework-5/C:\Golang_work\src\GoCourse\homework-5\test.txt:2: тест
