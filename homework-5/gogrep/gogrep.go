cb3d66c00bf6d9ece6a74702fe5108ac4eca88de
u2 <u2@lenta.ru>
2019-07-01 16:02:04 +0500

    gogrep.go has been modified

--- homework-5/gogrep/gogrep.go
+++ homework-5/gogrep/gogrep.go
@@ -1,27 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
-	query := os.Args[1]
-	root := os.Args[2]
+	query := os.Args[2]
+	root := os.Args[1]
+	found := 1
 
-	//arg := os.Args[3]
-
-	fmt.Println(query)
-	fmt.Println(root)
-	fmt.Println(os.Args)
+	fmt.Println("query", query)
+	fmt.Println("root", root)
+	//	fmt.Println("os.Args", os.Args)
 
 	filepath.Walk(root, func(path string, file os.FileInfo, err error) error {
+		//		fmt.Println(path)
 		if !file.IsDir() {
+			file, err := os.Open(path)
+			defer file.Close()
+			if err != nil {
+				return nil
+			}
+			scanner := bufio.NewScanner(file)
 			for i := 1; scanner.Scan(); i++ {
 				if strings.Contains(scanner.Text(), query) {
 					found = 0
 					fmt.Printf("%s/%s:%d: %s\n", root, path, i, scanner.Text())
 				}
+			}
 		}
 		return nil
+	})
+
+	/*	filepath.Walk(root, func(path string, file os.FileInfo, err error) error {
+			if !file.IsDir() {
+				for i := 1; scanner.Scan(); i++ {
+					if strings.Contains(scanner.Text(), query) {
+						found = 0
+						fmt.Printf("%s/%s:%d: %s\n", root, path, i, scanner.Text())
+					}
+			}
+			return nil
+		}
+		}
+	*/
 }
