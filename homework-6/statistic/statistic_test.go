a1417a9c127ba313066bc8340345452fcaf7bd77
u2 <u2@lenta.ru>
2019-07-03 18:56:20 +0500

    statistic.go, statistic_test.go has been modified

--- homework-6/statistic/statistic_test.go
+++ homework-6/statistic/statistic_test.go
@@ -9,3 +9,11 @@ func TestAverage(t *testing.T) {
 		t.Error("Expected 1.5 got ", v)
 	}
 }
+
+func TestSumxs(t *testing.T) {
+	var v float64
+	v = Sumxs([]float64{1, 2})
+	if v != 3 {
+		t.Error("Expected 3 got ", v)
+	}
+}
