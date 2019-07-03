b72e55d8729b4ab079f8effc62ea8534c94f70e5
u2 <u2@lenta.ru>
2019-07-03 22:40:24 +0500

    homework-6.go has been modified

--- homework-6/homework-6.go
+++ homework-6/homework-6.go
@@ -15,8 +15,17 @@ func main() {
 
 func pkgImg() {
 	green := color.RGBA{0, 255, 0, 255}
+	red := color.RGBA{200, 30, 30, 255}
 	rectImg := image.NewRGBA(image.Rect(0, 0, 200, 200))
 	draw.Draw(rectImg, rectImg.Bounds(), &image.Uniform{green}, image.ZP, draw.Src)
+	for x := 0; x <= 200; x++ {
+		y := 100
+		rectImg.Set(x, y, red)
+	}
+	for y := 0; y <= 200; y++ {
+		x := 100
+		rectImg.Set(x, y, red)
+	}
 	file, err := os.Create("rectangle.png")
 	if err != nil {
 		log.Fatalf("Failed create file: %s", err)
