7ca7f5650f0b3851e6c75b3885a1e4e68f9db825
u2 <u2@lenta.ru>
2019-07-03 22:26:00 +0500

    homework-6.go has been modified

--- homework-6/homework-6.go
+++ homework-6/homework-6.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"log"
+	"os"
+)
 
+func main() {
+	pkgImg()
+}
+
+func pkgImg() {
+	green := color.RGBA{0, 255, 0, 255}
+	rectImg := image.NewRGBA(image.Rect(0, 0, 200, 200))
+	draw.Draw(rectImg, rectImg.Bounds(), &image.Uniform{green}, image.ZP, draw.Src)
+	file, err := os.Create("rectangle.png")
+	if err != nil {
+		log.Fatalf("Failed create file: %s", err)
+	}
+	defer file.Close()
+	png.Encode(file, rectImg)
 }
