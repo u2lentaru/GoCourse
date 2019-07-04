f9fc0c62528394943506cf101736b794f48ab546
u2 <u2@lenta.ru>
2019-07-04 17:40:06 +0500

    helloGET() has been modified

--- homework-6/homework-6.go
+++ homework-6/homework-6.go
@@ -5,12 +5,18 @@ import (
 	"image/color"
 	"image/draw"
 	"image/png"
+	"io"
 	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 	pkgImg()
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/",fs)
+	http.HandleFunc("/helloGET", helloGET)
+	http.ListenAndServe(":80", nil)
 }
 
 func pkgImg() {
@@ -33,6 +39,16 @@ func pkgImg() {
 	defer file.Close()
 	png.Encode(file, rectImg)
 }
-func helloGET() {
-	
+
+func helloGET(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/html")
+	io.WriteString(res,
+		`<doctype html>
+		<html>
+		<title>Hello World!</title>
+		</head>
+		<body>
+		Hello World!
+		</body>
+		</html>`)
 }
