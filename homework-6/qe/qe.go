b05ecc7a8ba637fb2c58fa7358ca2e9028ac49f5
u2 <u2@lenta.ru>
2019-07-06 16:54:10 +0500

    qe.goб qe_test.go has been modified

--- homework-6/qe/qe.go
+++ homework-6/qe/qe.go
@@ -2,7 +2,7 @@ package qe
 
 import "math/cmplx"
 
-//QuadraticEquation solves the quadratic equation in complex valuesю
+//QuadraticEquation solves the quadratic equation in complex values
 func QuadraticEquation(a, b, c complex128) (x1, x2 complex128) {
 	sqd := cmplx.Sqrt(complex128(b*b - 4*a*c))
 	x1 = (-b + sqd) / (2 * a)
