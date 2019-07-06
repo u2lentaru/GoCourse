5602f1076f7a9f84feeee85956b8029f73083e91
u2 <u2@lenta.ru>
2019-07-06 16:37:38 +0500

    qe_test.go has been modified

--- homework-6/qe/qe_test.go
+++ homework-6/qe/qe_test.go
@@ -9,3 +9,11 @@ func TestQuadraticEquation(t *testing.T) {
 		t.Error("Expected 1,2 got", v1, v2)
 	}
 }
+
+//Running tool: C:\Go\bin\go.exe test -timeout 30s GoCourse\homework-6\qe -run ^(TestQuadraticEquation)$
+//
+//--- FAIL: TestQuadraticEquation (0.00s)
+//c:\Golang_work\src\GoCourse\homework-6\qe\qe_test.go:9: Expected 1,2 got (-1+1.4142135623730951i) (-1-1.4142135623730951i)
+//FAIL
+//FAIL	GoCourse/homework-6/qe	1.047s
+//Error: Tests failed.
