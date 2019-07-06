0361d0e509203625325fc1891e23667c817bb54f
u2 <u2@lenta.ru>
2019-07-06 16:46:19 +0500

    qe.goб qe_test.go has been modified

--- homework-6/qe/qe.go
+++ homework-6/qe/qe.go
@@ -2,7 +2,7 @@ package qe
 
 import "math/cmplx"
 
-//QuadraticEquation solving quadratic equation
+//QuadraticEquation solves the quadratic equation in complex valuesю
 func QuadraticEquation(a, b, c complex128) (x1, x2 complex128) {
 	sqd := cmplx.Sqrt(complex128(b*b - 4*a*c))
 	x1 = (-b + sqd) / (2 * a)
--- homework-6/qe/qe_test.go
+++ homework-6/qe/qe_test.go
@@ -5,15 +5,15 @@ import "testing"
 func TestQuadraticEquation(t *testing.T) {
 	var v1, v2 complex128
 	v1, v2 = QuadraticEquation(1, 2, 3)
-	if v1 != 1 && v2 != 1 {
-		t.Error("Expected 1,2 got", v1, v2)
+	if real(v1) != 1 && imag(v1) != 1 && real(v2) != 2 && imag(v2) != 2 {
+		t.Error("Expected 1+i,2+2i got", v1, v2)
 	}
 }
 
 //Running tool: C:\Go\bin\go.exe test -timeout 30s GoCourse\homework-6\qe -run ^(TestQuadraticEquation)$
 //
 //--- FAIL: TestQuadraticEquation (0.00s)
-//c:\Golang_work\src\GoCourse\homework-6\qe\qe_test.go:9: Expected 1,2 got (-1+1.4142135623730951i) (-1-1.4142135623730951i)
+//c:\Golang_work\src\GoCourse\homework-6\qe\qe_test.go:9: Expected 1+i,2+2i got (-1+1.4142135623730951i) (-1-1.4142135623730951i)
 //FAIL
-//FAIL	GoCourse/homework-6/qe	1.047s
+//FAIL	GoCourse/homework-6/qe	1.186s
 //Error: Tests failed.
