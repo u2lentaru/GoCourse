da18d9166170a8f2b6abcb600c2596672f5aaeed
u2 <u2@lenta.ru>
2019-07-06 16:27:24 +0500

    qe.go has been modified

--- homework-6/qe/qe.go
+++ homework-6/qe/qe.go
@@ -1,6 +1,11 @@
 package qe
 
-func quadraticEquation(q, b, c int) (x1, x2 float64) {
+import "math/cmplx"
 
+//QuadraticEquation solving quadratic equation
+func QuadraticEquation(a, b, c complex128) (x1, x2 complex128) {
+	sqd := cmplx.Sqrt(complex128(b*b - 4*a*c))
+	x1 = (-b + sqd) / (2 * a)
+	x2 = (-b - sqd) / (2 * a)
 	return x1, x2
 }
