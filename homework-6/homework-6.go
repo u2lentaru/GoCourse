6fff11d1cc3bb3473eac0a159f75ed08e72d1751
u2 <u2@lenta.ru>
2019-07-08 17:35:38 +0500

    homework-6.go has been modified

--- homework-6/homework-6.go
+++ homework-6/homework-6.go
@@ -20,6 +20,7 @@ func main() {
 	http.ListenAndServe(":80", nil)
 }
 
+//Generates a png-file with rectangle with vertical and horizontal lines
 func pkgImg() {
 	green := color.RGBA{0, 255, 0, 255}
 	red := color.RGBA{200, 30, 30, 255}
@@ -41,6 +42,7 @@ func pkgImg() {
 	png.Encode(file, rectImg)
 }
 
+//Displaying GET parameter 'name'
 func helloGET(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "text/html")
 	getName := req.URL.Query().Get("name")
@@ -48,36 +50,34 @@ func helloGET(res http.ResponseWriter, req *http.Request) {
 		`<doctype html>
 		<html>
 		<title>Hello World!</title>
-		</head>
-		<body>
+		</head><body>
 		Hello World!
 		<br>`+getName+`</body>
 		</html>`)
 }
 
+//chessBoard() generates a png-file with a chessboard pattern
 func chessBoard() {
 	myimage := image.NewRGBA(image.Rect(0, 0, 240, 240))
 	colors := make(map[int]color.RGBA, 2)
 
-	colors[0] = color.RGBA{0, 100, 0, 255}   // green
-	colors[1] = color.RGBA{50, 205, 50, 255} // limegreen
+	colors[0] = color.RGBA{139, 69, 19, 200}   //brown
+	colors[1] = color.RGBA{139, 139, 122, 200} //yellow
 
 	indexColor := 0
 	locX := 0
 
 	for currX := 0; currX < 8; currX++ {
-
 		locY := 0
 		for currY := 0; currY < 8; currY++ {
 
 			draw.Draw(myimage, image.Rect(locX, locY, locX+30, locY+30),
 				&image.Uniform{colors[indexColor]}, image.ZP, draw.Src)
-
 			locY += 30
-			indexColor = 1 - indexColor // toggle from 0 to 1 to 0 to 1 to ...
+			indexColor = 1 - indexColor // toggle indexColor 0,1
 		}
 		locX += 30
-		indexColor = 1 - indexColor // toggle from 0 to 1 to 0 to 1 to ...
+		indexColor = 1 - indexColor // toggle indexColor 0,1
 	}
 	myfile, err := os.Create("chessboard.png")
 	if err != nil {
